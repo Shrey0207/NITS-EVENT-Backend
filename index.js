3211b7d0aab86ffba5bd5547c24e8d3f546fd236
Shrey0207 <shreytolasaria4297@gmail.com>
2024-06-30 01:26:03 +0530

    implemented validate token api

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { connectMongoDB } = require("./config/db-config");
+const cookieParser = require("cookie-parser");
 const app = express();
 require("dotenv").config();
 
 connectMongoDB();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/users", require("./routes/users-route"));
 
 const port = process.env.PORT || 5000;
