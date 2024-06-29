41f85737cb90f9f8a27a4dee4e9675a92019d272
Shrey0207 <shreytolasaria4297@gmail.com>
2024-06-29 23:41:55 +0530

    implemented user login api

--- routes/users-route.js
+++ routes/users-route.js
@@ -23,4 +23,32 @@ router.post("/register", async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 });
+
+// user login
+router.post("/login", async (req, res) => {
+  try {
+    // check if user exists
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    // check if password is correct
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    // create and assign a token
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET_KEY);
+
+    return res.status(200).json({ token, message: "Login successfull" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = router;
